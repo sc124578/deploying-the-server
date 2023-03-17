42aa653bd257e0fa9b6c30310d795a19d053035b
Scott Knell <scottknell455@hotmail.com>
2023-03-17 03:14:17 -0600

    new

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const router = express.Router();
 const PORT = process.env.PORT || 5001;
 
 router.get('/', cors(), (req, res) => {
-  res.json({ message: 'Hello Render!' });
+  res.json({ message: 'This is a message from me to you!' });
 });
 
 app.use('/', router);
